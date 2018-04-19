b0638d496604f6b32d20978c6ef2cd38bb44a603
DanMcCabeSD <danieljm@udel.edu>
2018-04-18 20:32:01 -0700

    last edit i hope

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,7 @@
 // ===============================================================================
 var path = require("path");
 
+module.exports = function(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
@@ -13,3 +14,4 @@ var path = require("path");
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+}
